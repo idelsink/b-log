2761dc5c78d0c4df6914c2d8f50e579fac691fcc
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-07-01 15:57:48 +0200

    add output to file option

--- b-log.sh
+++ b-log.sh
@@ -13,8 +13,8 @@ set -e          # kill script if a command fails
 set -o nounset  # unset values give error
 set -o pipefail # prevents errors in a pipeline from being masked
 
-VERSION_B_LOG=0.0.1
-APPNAME_B_LOG="b-log"
+B_LOG_VERSION=0.0.1
+B_LOG_APPNAME="b-log"
 
 # --- global variables ----------------------------------------------
 # log levels
@@ -41,12 +41,12 @@ readonly LOG_LEVEL_ALL=700      # all enabled
 B_LOG_DEFAULT_TEMPLATE=( "[@1@][@2@][@3@:@4@] @5@" ) # default template
 # level code, level name, level template, prefix(colors etc.), suffix(colors etc.)
 LOG_LEVELS=(
-    ${LOG_LEVEL_FATAL}  "FATAL " "${B_LOG_DEFAULT_TEMPLATE}" "\e[41;37m" "\e[0m"
-    ${LOG_LEVEL_ERROR}  "ERROR " "${B_LOG_DEFAULT_TEMPLATE}" "\e[1;31m" "\e[0m"
-    ${LOG_LEVEL_WARN}   "WARN  " "${B_LOG_DEFAULT_TEMPLATE}" "\e[1;33m" "\e[0m"
-    ${LOG_LEVEL_INFO}   "INFO  " "${B_LOG_DEFAULT_TEMPLATE}" "\e[37m" "\e[0m"
-    ${LOG_LEVEL_DEBUG}  "DEBUG " "${B_LOG_DEFAULT_TEMPLATE}" "\e[1;34m" "\e[0m"
-    ${LOG_LEVEL_TRACE}  "TRACE " "${B_LOG_DEFAULT_TEMPLATE}" "\e[94m" "\e[0m"
+    ${LOG_LEVEL_FATAL}  "FATAL" "${B_LOG_DEFAULT_TEMPLATE}" "\e[41;37m" "\e[0m"
+    ${LOG_LEVEL_ERROR}  "ERROR" "${B_LOG_DEFAULT_TEMPLATE}" "\e[1;31m" "\e[0m"
+    ${LOG_LEVEL_WARN}   "WARN " "${B_LOG_DEFAULT_TEMPLATE}" "\e[1;33m" "\e[0m"
+    ${LOG_LEVEL_INFO}   "INFO " "${B_LOG_DEFAULT_TEMPLATE}" "\e[37m" "\e[0m"
+    ${LOG_LEVEL_DEBUG}  "DEBUG" "${B_LOG_DEFAULT_TEMPLATE}" "\e[1;34m" "\e[0m"
+    ${LOG_LEVEL_TRACE}  "TRACE" "${B_LOG_DEFAULT_TEMPLATE}" "\e[94m" "\e[0m"
 )
 # log levels columns
 readonly LOG_LEVELS_LEVEL=0
@@ -56,6 +56,11 @@ readonly LOG_LEVELS_PREFIX=$((LOG_LEVELS_TEMPLATE+1))
 readonly LOG_LEVELS_SUFFIX=$((LOG_LEVELS_PREFIX+1))
 
 LOG_LEVEL=${LOG_LEVEL_WARN} # current log level
+B_LOG_LOG_VIA_STDOUT=true       # log via stdout
+B_LOG_LOG_VIA_FILE=""           # file if logging via file (file, add suffix, add prefix)
+B_LOG_LOG_VIA_FILE_PREFIX=false
+B_LOG_LOG_VIA_FILE_SUFFIX=false
+B_LOG_LOG_VIA_SYSLOG=""           #
 B_LOG_TS=""                 # timestamp variable
 B_LOG_LOG_LEVEL_NAME=""     # the level name message
 B_LOG_LOG_MESSAGE=""        # the log message
@@ -70,6 +75,13 @@ function B_LOG(){
         echo "Usage: command -hVo"
         echo "-h --help help"
         echo "-V --version version"
+        echo "-o --stdout 'false/true' (default true)"
+        echo "-f --file 'file'"
+        echo "--file-prefix-enable enable the prefix for the log file"
+        echo "--file-prefix-disable disable the prefix for the log file"
+        echo "--file-suffix-enable enable the suffix for the log file"
+        echo "--file-suffix-disable disable the suffix for the log file"
+        echo "-s --syslog 'xxx'"
         echo "-l --log-level the level of the log"
         echo "  Log levels      : value"
         echo " ---------------- : -----"
@@ -80,7 +92,6 @@ function B_LOG(){
         echo "  LOG_LEVEL_INFO  : ${LOG_LEVEL_INFO}"
         echo "  LOG_LEVEL_DEBUG : ${LOG_LEVEL_DEBUG}"
         echo "  LOG_LEVEL_TRACE : ${LOG_LEVEL_TRACE}"
-        echo "-o --output destination/output directory"
         echo ""
     }
     for arg in "$@"; do # transform long options to short ones
@@ -89,19 +100,56 @@ function B_LOG(){
             "--help") set -- "$@" "-h" ;;
             "--version") set -- "$@" "-V" ;;
             "--log-level") set -- "$@" "-l" ;;
-            "--output") set -- "$@" "-o" ;;
+            "--stdout") set -- "$@" "-o" ;;
+            "--file") set -- "$@" "-f" ;;
+            "--file-prefix-enable") set -- "$@" "-a" "file-prefix-enable" ;;
+            "--file-prefix-disable") set -- "$@" "-a" "file-prefix-disable" ;;
+            "--file-suffix-enable") set -- "$@" "-a" "file-suffix-enable" ;;
+            "--file-suffix-disable") set -- "$@" "-a" "file-suffix-disable" ;;
+            "--syslog") set -- "$@" "-s" ;;
             *) set -- "$@" "$arg"
       esac
     done
     # get options
-    while getopts "hVl:" optname
+    while getopts "hVo:f:s:l:a:" optname
     do
         case "$optname" in
             "h")
                 PRINT_USAGE
                 ;;
             "V")
-                echo "${APPNAME_B_LOG} v${VERSION_B_LOG}"
+                echo "${B_LOG_APPNAME} v${B_LOG_VERSION}"
+                ;;
+            "o")
+                if [ "${OPTARG}" = true ]; then
+                    B_LOG_LOG_VIA_STDOUT=true
+                else
+                    B_LOG_LOG_VIA_STDOUT=false
+                fi
+                ;;
+            "f")
+                B_LOG_LOG_VIA_FILE=${OPTARG}
+                ;;
+            "a")
+                case ${OPTARG} in
+                    'file-prefix-enable' )
+                        B_LOG_LOG_VIA_FILE_PREFIX=true
+                        ;;
+                    'file-prefix-disable' )
+                        B_LOG_LOG_VIA_FILE_PREFIX=false
+                        ;;
+                    'file-suffix-enable' )
+                        B_LOG_LOG_VIA_FILE_SUFFIX=true
+                        ;;
+                    'file-suffix-disable' )
+                        B_LOG_LOG_VIA_FILE_SUFFIX=false
+                        ;;
+                    *)
+                        ;;
+                esac
+                ;;
+            "s")
+                B_LOG_LOG_VIA_SYSLOG=${OPTARG}
                 ;;
             "l")
                 LOG_LEVEL=${OPTARG}
@@ -167,7 +215,7 @@ function B_LOG_convert_template() {
                 log_layout_part="${FUNCNAME[2]}"
                 ;;
             4) # line number
-                log_layout_part="${BASH_LINENO[0]}"
+                log_layout_part="${BASH_LINENO[1]}"
                 ;;
             5) # message
                 log_layout_part="${B_LOG_LOG_MESSAGE}"
@@ -199,15 +247,48 @@ function B_LOG_MESSAGE() {
     fi
     shift
     local message=${@:-}
+    # if message is empty, get from stdin
     if [ -z "$message" ]; then
         message="$(cat /dev/stdin)"
     fi
     B_LOG_LOG_MESSAGE="${message}"
     B_LOG_get_log_level_info "${log_level}" || true
     B_LOG_convert_template ${LOG_FORMAT} || true
-    echo -ne "$LOG_PREFIX"
-    echo -ne "${B_LOG_CONVERTED_TEMPLATE_STRING}"
-    echo -e "$LOG_SUFFIX"
+    # output to stdout
+    if [ "${B_LOG_LOG_VIA_STDOUT}" = true ]; then
+        echo -ne "$LOG_PREFIX"
+        echo -ne "${B_LOG_CONVERTED_TEMPLATE_STRING}"
+        echo -e "$LOG_SUFFIX"
+    fi
+    # output to file
+    if [ ! -z "${B_LOG_LOG_VIA_FILE}" ]; then
+        if [ ! -d "${B_LOG_LOG_VIA_FILE%/*}" ]; then
+            # directory does not exist
+            mkdir -p "${B_LOG_LOG_VIA_FILE%/*}" || true
+        else
+            if [ ! -e "${B_LOG_LOG_VIA_FILE}" ]; then
+                # file does not exist
+                touch "${B_LOG_LOG_VIA_FILE}" || true
+            else
+                message=""
+                if [ "${B_LOG_LOG_VIA_FILE_PREFIX}" = true ]; then
+                    message="${message}${LOG_PREFIX}"
+                fi
+                message="${message}${B_LOG_CONVERTED_TEMPLATE_STRING}"
+                if [ "${B_LOG_LOG_VIA_FILE_SUFFIX}" = true ]; then
+                    message="${message}${LOG_SUFFIX}"
+                fi
+                echo -e "${message}" >> ${B_LOG_LOG_VIA_FILE} || true
+            fi
+        fi
+    fi
+    # output to syslog
+    if [ ! -z "${B_LOG_LOG_VIA_SYSLOG}" ]; then
+        # got syslog
+        :
+    fi
+
+
 }
 
 # set alias for log level command
